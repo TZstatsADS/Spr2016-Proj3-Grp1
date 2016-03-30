b6be28815473e752d6f1a47d6342b7014e4ac589
ZacWu <zw2338@columbia.edu>
2016-03-30 15:33:11 -0400

    updates
    1.train
    2.test

--- lib/test.R
+++ lib/test.R
@@ -2,25 +2,30 @@
 ### Fit the classification model with testing data ###
 ######################################################
 
-### Author: Yuting Ma
+### Author: Yuting Ma  edited by: QS
 ### Project 3
 ### ADS Spring 2016
 
-test <- function(fit_train, dat_test){
-  
-  ### Fit the classfication model with testing data
-  
-  ### Input: 
-  ###  - the fitted classification model using training data
-  ###  -  processed features from testing images 
-  ### Output: training model specification
-  
-  ### load libraries
-  library("gbm")
-  
-  pred <- predict(fit_train$fit, newdata=dat_test, 
-                  n.trees=fit_train$iter, type="response")
-  
-  return(as.numeric(pred> 0.5))
-}
 
+test <- function(fit_train, dat_test){
+    package_need<-c("png","xgboost","data.table","e1071")
+    for (i in 1:length(package_need)){
+        if( package_need[i] %in% rownames(installed.packages()) == FALSE)
+        install.packages(package_need[i])
+    }
+    library(png)
+    library(xgboost)
+    library(data.table)
+    library(e1071)
+    
+    
+    mat.test<-as.matrix(dat_test)
+    g.test<-apply(mat.test,2,as.numeric)
+    g.test[is.na(g.test)]<-0
+    color<-g.test[,1001:1360]
+    sift<-g.test[,1:1360]
+    pred_baseline <- predict(fit_train$svmfit[[1]],newdata=color)
+    pred_ad <- predict(fit_train$gboostfit,newdata=sift)
+    return (list(baseline=pred_baseline, adv=as.numeric(pred_ad>0.5)))
+    
+}
--- lib/train.R
+++ lib/train.R
@@ -2,19 +2,66 @@
 ### Train a classification model with training images ###
 #########################################################
 
-### Author: Yuting Ma
+### Author: Yuting Ma    edited by: QS
 ### Project 3
 ### ADS Spring 2016
 
 
-train <- function(train_X, labels, par=NULL){
 
-        library(e1071)
-        names(labels) <- c("labels")
-        traindata = cbind(labels,train_X)
-        svm_fit <- svm(labels~.,kernel = "linear",data = traindata,gamma=0,cost = 0.27,type="C") 
-        ### Train with gradient boosting model
-  
-
-  return(list(fit=svm_fit))
+train <- function(train_X, labels, nround, par=NULL){
+    
+    package_need<-c("png","xgboost","data.table","e1071")
+    for (i in 1:length(package_need)){
+        if( package_need[i] %in% rownames(installed.packages()) == FALSE)
+        install.packages(package_need[i])
+    }
+    library(png)
+    library(xgboost)
+    library(data.table)
+    library(e1071)
+    
+    train.label<-labels
+    
+    length<-length(train_X)
+    
+    mat.train<-as.matrix(train_X)
+    
+    
+    g.train<-apply(mat.train,2,as.numeric)
+    g.train[is.na(g.train)]<-0
+    
+    
+    
+    ##begin train##
+    numberOfClasses <- max(train.label) + 1
+    
+    param <- list("objective" = "multi:softprob",
+    "eval_metric" = "logloss"
+    )
+    
+    #cv.nround <- 50
+    #cv.nfold <- 5
+    #bst.cv = xgb.cv(param=param, data = g.train, label = train.label,
+    #               nfold = cv.nfold, nrounds = cv.nround, max.depth=2)
+    
+    nround <- 48
+    bst = xgboost(param=param, data =g.train[,1:1360], label = train.label, nrounds=nround,objective = "binary:logistic",max.depth=2)
+    svm <- svm_train(g.train[,1001:1360],labels)
+    
+    return(list(svmfit=svm, gboostfit=bst))
+    
 }
+
+
+svm_train <- function(train_X, labels, par=NULL){
+    
+    names(labels) <- c("labels")
+    traindata = cbind(labels,train_X)
+    svm_fit <- svm(labels~.,kernel = "linear",data = traindata,gamma=0,cost = 0.27,type="C")
+    ### Train with gradient boosting model
+    
+    
+    return(list(fit=svm_fit))
+}
+
+
